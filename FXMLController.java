015e51d527f41426237417f122108818af326e52
EmmaNiu9403 <Emma9403@126.com>
2016-08-02 19:35:05 -0500

    changes
    changes

--- FXMLController.java
+++ FXMLController.java
@@ -1,115 +1,49 @@
 package application;
 
 import java.net.URL;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.HashSet;
 import java.util.ResourceBundle;
-import java.util.Set;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.jar.JarInputStream;
+import java.util.Scanner;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.lang.reflect.*;
+public class FXMLController implements Initializable {
+	@FXML
+	private ChoiceBox<String> critters;
+	@FXML
+	private Spinner<Integer> number;
+	@FXML
+	private GridPane CritterPane;
+	@FXML
+	private Text stats;
+	@FXML
+	private TextField steps;
+	@FXML
+	private Button start;
 
-public class FXMLController implements Initializable 
-{
-	ArrayList<String> list = new ArrayList<String>();
-	ObservableList<String> makeCritterList;
-
-	@FXML 
-	private ChoiceBox<String> makeCritterType;
-	
 	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) 
-	{
-		assert makeCritterType != null : "fx:id=\"makeCritterType\" was not injected: check your FXML file 'FXMLgui.fxml'.";		
-		
-		makeCritterType.setValue("Choose a Critter");
-		try {
-			createListOfCritter();
-			makeCritterList = FXCollections.observableList(list);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		makeCritterType.setItems(makeCritterList);
-		
-	}
-	
-	private void createListOfCritter() throws Exception
-	{
-        String dir = System.getProperty("user.dir");
-        File file = new File(dir + "/bin/application");
-        File[] files = file.listFiles(new ClassFileFilter());
-        for(File f: files)
-        {
-        	if(f.canRead() && (!(f.isDirectory())))
-        	{
-        		String[] filenames =f.getName().split(".class");
-        		String filename = "application." + filenames[0];
-        		System.out.println(filename);
-        		Class<?> critterName = Class.forName(filename);
-        		Class critter = Class.forName("application.Critter");
-        		
-				if(!(critterName.isInterface()) && !(Modifier.isAbstract( critterName.getModifiers() )) && critter.isAssignableFrom(critterName))
-        		{
-        			list.add(filename);
-        		}
-        	}
-        }
-	}
-	
-	
-//	// fx:id="myButton1"
-//	@FXML private Button myButton1; // Value injected by FXMLLoader
-//
-//	// fx:id="myChoiceBox1"
-//	@FXML private ComboBox myChoiceBox1;
-//	
-//	@Override
-//	public void initialize(URL arg0, ResourceBundle arg1) {
-//		assert myButton1 != null : "fx:id=\"myButton1\" was not injected: check your FXML file 'FXMLgui.fxml'.";		
-//
-//		// initialize your logic here: all @FXML variables will have been injected
-//		myButton1.setOnAction(new EventHandler<ActionEvent>() {
-//			@Override
-//			public void handle(ActionEvent event) {
-//				System.out.println("That was easy, wasn't it?");
-//			}
-//		});
-//		
-//		assert myChoiceBox1 != null : "fx:id=\"choiceBox\" was not injected: check your FXML file 'FXMLgui.fxml'.";		
-//	
-//	}
-	
-	private class ClassFileFilter implements FileFilter
-	{
-	  private final String[] okFileExtensions = 
-	    new String[] {"class"};
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
 
-	  public boolean accept(File file)
-	  {
-	    for (String extension : okFileExtensions)
-	    {
-	      if (file.getName().endsWith(extension))
-	      {
-	        return true;
-	      }
-	    }
-	    return false;
-	  }
+		ObservableList<String> critterList = FXCollections.observableArrayList("Craig", "Algae", "Emma", "Niu", "Gary",
+				"Bob");
+		critters.setItems(critterList);
+		SpinnerValueFactory<Integer> svf = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 1000000);
+		number.setValueFactory(svf);
+		number.setEditable(true);
+		CritterPane.setMaxWidth(new Double(Params.world_width));
+		CritterPane.setMaxHeight(new Double(Params.world_height));
+		stats.setText("stats");	
+		
 	}
-	
+
 }
